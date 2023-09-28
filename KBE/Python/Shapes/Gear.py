e4a35b01d46c2636c56e9208f77e70f31e23497d
Einar Myhrvold <einartmy@ntnu.no>
2023-09-28 13:54:23 +0200

    Gear class that works

--- KBE/Python/Shapes/Gear.py
+++ KBE/Python/Shapes/Gear.py
@@ -5,48 +5,26 @@ import math
 
 class Gear:
 	
-    def _init_(self, gearR, toothL, x, y):
-		self.gearR = gearR
-		self.toothL = toothL
-		self.x = x
-		self.y = y
-		
-        teeth = math.floor(gear_radius / 0.8)
+    def __init__(self, gearR, toothL, gearHeight, x, y):
+        self.gearR = gearR
+        self.toothL = toothL
+        self.x = x
+        self.y = y
+        
+        teeth = math.floor(gearR / 4)
         angle = 2*math.pi / teeth
-        cylinder_radius = gear_radius-tooth_length
-        cylinder_height = cylinder_radius/5
-        gear = Cylinder(x,y,0, cylinder_radius * 2, cylinder_height,  [0,0,1])
+        cylinder_radius = gearR-toothL
+        gear = Cylinder(x,y,0, cylinder_radius * 2, gearHeight,  [0,0,1])
         for i in range(0, teeth):
-            tooth = Block_2(x + (cylinder_radius-0.1*tooth_length)*math.cos(i*angle),
-                y + (cylinder_radius-0.1*tooth_length)*math.sin(i*angle),
+            tooth = Block_2(x + (cylinder_radius-0.1*toothL)*math.cos(i*angle),
+                y + (cylinder_radius-0.1*toothL)*math.sin(i*angle),
                 0,
-                tooth_length,
-                tooth_length,
-                cylinder_height,
+                toothL,
+                toothL,
+                gearHeight,
                 NXOpen.Vector3d(math.cos(i*angle), math.sin(i*angle), 0.0),
                 NXOpen.Vector3d(math.cos(i*angle+math.pi/2), math.sin(i*angle+math.pi/2),0.0))
             gear.unite(tooth)
-        hole_in_gear = Cylinder(x, y, 0, cylinder_radius/15, cylinder_height,[0,0,1])
+        hole_in_gear = Cylinder(x, y, 0, cylinder_radius/15, gearHeight,[0,0,1])
         gear.subtract(hole_in_gear)
-		
-
-def Gear (gear_radius, tooth_length, x, y):
-	teeth = math.floor(gear_radius / 0.8)
-	angle = 2*math.pi / teeth
-	cylinder_radius = gear_radius-tooth_length
-	cylinder_height = cylinder_radius/5
-	gear = Cylinder(x,y,0, cylinder_radius * 2, cylinder_height,  [0,0,1])
-	for i in range(0, teeth):
-		tooth = Block_2(x + (cylinder_radius-0.1*tooth_length)*math.cos(i*angle),
-            y + (cylinder_radius-0.1*tooth_length)*math.sin(i*angle),
-			0,
-			tooth_length,
-			tooth_length,
-			cylinder_height,
-			NXOpen.Vector3d(math.cos(i*angle), math.sin(i*angle), 0.0),
-			NXOpen.Vector3d(math.cos(i*angle+math.pi/2), math.sin(i*angle+math.pi/2),0.0))
-		gear.unite(tooth)
-	hole_in_gear = Cylinder(x, y, 0, cylinder_radius/15, cylinder_height,[0,0,1])
-	gear.subtract(hole_in_gear)
-
-	return gear
+            
--- KBE/Python/gear_journal.py
+++ KBE/Python/gear_journal.py
@@ -1,3 +1,3 @@
 from Shapes.Gear import Gear
 
-gear1 = Gear(100, 10, 0, 0)
+gear1 = Gear(100, 20, 20, 0, 400)
