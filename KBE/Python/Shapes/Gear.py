abda7e151d71155d16af017294fcca05cb3d7882
Einar Myhrvold <einartmy@ntnu.no>
2023-09-29 12:07:16 +0200

    Fixing gear teeth to follow the wheel and adding width argument

--- KBE/Python/Shapes/Gear.py
+++ KBE/Python/Shapes/Gear.py
@@ -5,26 +5,27 @@ import math
 
 class Gear:
 	
-    def __init__(self, gearR, toothL, gearHeight, x, y):
+    def __init__(self, gearR, toothLength, toothWidth, gearHeight, x, y):
         self.gearR = gearR
-        self.toothL = toothL
+        self.toothLength = toothLength
+        self.toothWidth = toothWidth
         self.gearHeight = gearHeight
         self.x = x
         self.y = y
         
-        teeth = math.floor(gearR / 6)
-        angle = 2*math.pi / teeth
-        cylinder_radius = gearR-toothL
+        teeth = math.ceil(math.pi*gearR // toothWidth*0.9)
+        angle = 2*math.pi / (teeth)
+        cylinder_radius = gearR-toothLength
         gear = Cylinder(x,y,0, cylinder_radius * 2, gearHeight,  [0,0,1])
         for i in range(0, teeth):
-            tooth = Block_2(x + (cylinder_radius-0.1*toothL)*math.cos(i*angle),
-                y + (cylinder_radius-0.1*toothL)*math.sin(i*angle),
+            tooth = Block_2(x + (cylinder_radius-0.1*toothLength)*math.cos(i*angle),
+                y + (cylinder_radius-0.1*toothLength)*math.sin(i*angle),
                 0,
-                toothL,
-                toothL,
+                toothLength,
+                toothWidth,
                 gearHeight,
-                NXOpen.Vector3d(math.cos(i*angle), math.sin(i*angle), 0.0),
-                NXOpen.Vector3d(math.cos(i*angle+math.pi/2), math.sin(i*angle+math.pi/2),0.0))
+                NXOpen.Vector3d(math.cos((i+15/gearR)*angle),              math.sin((i+15/gearR)*angle),              0.0),
+                NXOpen.Vector3d(math.cos((i+15/gearR)*angle +math.pi/2),   math.sin((i+15/gearR)*angle +math.pi/2),   0.0))
             gear.unite(tooth)
         hole_in_gear = Cylinder(x, y, 0, cylinder_radius/15, gearHeight,[0,0,1])
         gear.subtract(hole_in_gear)
--- KBE/Python/gear_journal.py
+++ KBE/Python/gear_journal.py
@@ -1,4 +1,4 @@
 from Shapes.Gear import Gear
 
-gear1 = Gear(100, 15, 40, 0, 0)
-gear2 = Gear(gear1.gearR, gear1.toothL, gear1.gearHeight, gear1.x, -2*gear1.gearR + gear1.toothL*0.8)
+gear1 = Gear(70, 10, 10, 20, 0, 0)
+gear2 = Gear(gear1.gearR, gear1.toothLength, gear1.toothWidth, gear1.gearHeight, gear1.x, -2*gear1.gearR + gear1.toothLength*0.7)
