ccac8db6b9a5dd431357a46dd1d5daea9ed42144
Martin Bergstø <mbergst@ntnu.no>
2023-09-29 14:29:51 +0200

    Added lower case

--- KBE/Python/case.py
+++ KBE/Python/case.py
@@ -8,8 +8,7 @@ from Shapes.Swept import Swept
 from Shapes.Line import Line 
 from Shapes.Arc import Arc
 
-class Case:
-
+class UpperCase:
 
     def __init__(self, gear_center_x, gear_center_y, gear_radius, depth):
         self.depth = depth
@@ -17,34 +16,95 @@ class Case:
         self.gear_center_y = gear_center_y
         self.gear_radius = gear_radius
 
-        line1 = Line(gear_center_x - gear_radius - 10,gear_center_y - gear_radius*0.5,0,gear_center_x - gear_radius - 10, gear_center_y,0)
-        arc_g1 = Arc(gear_center_x, gear_center_y,0,(1,0,0),(0,1,0),gear_radius + 10,0, 180)
-        line2 = Line(gear_center_x + gear_radius + 10, gear_center_y - gear_radius*0.5,0,gear_center_x + gear_radius + 10,gear_center_y,0)
+        def create_line(x_start, y_start, x_end, y_end, depth_start, depth_end):
+            return Line(x_start, y_start, depth_start, x_end, y_end, depth_end)
+        
+        def create_swept(main_line, base_lines):
+            return Swept([main_line], base_lines)
+        
+        half_gear_radius = gear_radius * 0.5
+        y_position = gear_center_y - half_gear_radius
 
-        base_line1 = Line(gear_center_x - gear_radius - 15, gear_center_y - 0.5*gear_radius, depth, gear_center_x - gear_radius - 5,gear_center_y - 0.5*gear_radius, depth)
-        base_line2 = Line(gear_center_x - gear_radius - 5,gear_center_y - 0.5*gear_radius, depth, gear_center_x - gear_radius - 5,gear_center_y - 0.5*gear_radius,0)
-        base_line3 = Line(gear_center_x - gear_radius - 5,gear_center_y - 0.5*gear_radius,0,gear_center_x - gear_radius - 15,gear_center_y - 0.5*gear_radius,0)
-        base_line4 = Line(gear_center_x - gear_radius - 15,gear_center_y - 0.5*gear_radius,0, gear_center_x - gear_radius - 15,gear_center_y - 0.5*gear_radius, depth)
+        # Create first swept object
+        line1 = create_line(gear_center_x - gear_radius - 10, y_position, gear_center_x - gear_radius - 10, gear_center_y, 0, 0)
+        arc_g1 = Arc(gear_center_x, gear_center_y, 0, (1, 0, 0), (0, 1, 0), gear_radius + 10, 0, 180)
+        line2 = create_line(gear_center_x + gear_radius + 10, y_position, gear_center_x + gear_radius + 10, gear_center_y, 0, 0)
+        base_lines = [
+            create_line(gear_center_x - gear_radius - 15, y_position, gear_center_x - gear_radius - 5, y_position, depth, depth),
+            create_line(gear_center_x - gear_radius - 5, y_position, gear_center_x - gear_radius - 5, y_position, depth, 0),
+            create_line(gear_center_x - gear_radius - 5, y_position, gear_center_x - gear_radius - 15, y_position, 0, 0),
+            create_line(gear_center_x - gear_radius - 15, y_position, gear_center_x - gear_radius - 15, y_position, 0, depth)
+        ]
+        swept = Swept([line1, arc_g1, line2], base_lines)
 
-        swept = Swept([line1, arc_g1, line2], [base_line1,base_line2,base_line3,base_line4])
+        # Create second swept object
+        line3 = create_line(gear_center_x - gear_radius - 5, y_position, gear_center_x - gear_radius - 110, y_position, 0, 0)
+        base_lines = [
+            create_line(gear_center_x - gear_radius - 5, y_position + 10, gear_center_x - gear_radius - 5, y_position + 10, 0, depth),
+            create_line(gear_center_x - gear_radius - 5, y_position + 10, gear_center_x - gear_radius - 5, y_position, depth, depth),
+            create_line(gear_center_x - gear_radius - 5, y_position, gear_center_x - gear_radius - 5, y_position, depth, 0),
+            create_line(gear_center_x - gear_radius - 5, y_position, gear_center_x - gear_radius - 5, y_position + 10, 0, 0)
+        ]
+        swept2 = create_swept(line3, base_lines)
 
-        line3 = Line(gear_center_x - gear_radius -5,gear_center_y - gear_radius*0.5,0,gear_center_x - gear_radius -110, gear_center_y - gear_radius*0.5,0)
+        # Create third swept object
+        line4 = create_line(gear_center_x + gear_radius + 5, y_position, gear_center_x + gear_radius + 110, y_position, 0, 0)
+        base_lines = [
+            create_line(gear_center_x + gear_radius + 5, y_position + 10, gear_center_x + gear_radius + 5, y_position + 10, 0, depth),
+            create_line(gear_center_x + gear_radius + 5, y_position + 10, gear_center_x + gear_radius + 5, y_position, depth, depth),
+            create_line(gear_center_x + gear_radius + 5, y_position, gear_center_x + gear_radius + 5, y_position, depth, 0),
+            create_line(gear_center_x + gear_radius + 5, y_position, gear_center_x + gear_radius + 5, y_position + 10, 0, 0)
+        ]
+        swept3 = create_swept(line4, base_lines)
 
-        base_line1 = Line(gear_center_x - gear_radius -5,gear_center_y - gear_radius*0.5 + 10,0,gear_center_x - gear_radius -5,gear_center_y - gear_radius*0.5 + 10,depth)
-        base_line2 = Line(gear_center_x - gear_radius -5,gear_center_y - gear_radius*0.5 + 10,depth,gear_center_x - gear_radius -5,gear_center_y - gear_radius*0.5,depth)
-        base_line3 = Line(gear_center_x - gear_radius -5,gear_center_y - gear_radius*0.5,depth,gear_center_x - gear_radius -5,gear_center_y - gear_radius*0.5,0)
-        base_line4 = Line(gear_center_x - gear_radius -5,gear_center_y - gear_radius*0.5,0,gear_center_x - gear_radius -5,gear_center_y - gear_radius*0.5 + 10,0)
 
-        swept2 = Swept([line3], [base_line1,base_line2,base_line3,base_line4])
+class LowerCase:
 
-        line4 = Line(gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5, 0, gear_center_x + gear_radius + 110, gear_center_y - gear_radius*0.5 ,0)
+    def __init__(self, gear_center_x, gear_center_y, gear_radius, depth):
+        self.depth = depth
+        self.gear_center_x = gear_center_x
+        self.gear_center_y = gear_center_y
+        self.gear_radius = gear_radius
 
-        base_line1 = Line(gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5 + 10,0,gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5 + 10, depth)
-        base_line2 = Line(gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5 + 10, depth,gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5, depth )
-        base_line3 = Line(gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5, depth,gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5,0)
-        base_line4 = Line(gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5,0,gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5 + 10,0)
+        def create_line(x_start, y_start, x_end, y_end, depth_start, depth_end):
+            return Line(x_start, y_start, depth_start, x_end, y_end, depth_end)
 
-        swept3 = Swept([line4], [base_line1,base_line2,base_line3,base_line4])
+        def create_swept(main_line, base_lines):
+            return Swept([main_line], base_lines)
 
+        half_gear_radius = gear_radius * 0.5
+        y_position = gear_center_y + half_gear_radius  # changed to plus for mirroring
+
+        # Create first swept object
+        line1 = create_line(gear_center_x - gear_radius - 10, y_position, gear_center_x - gear_radius - 10, gear_center_y, 0, 0)  # Keep x, Change y
+        arc_g1 = Arc(gear_center_x, gear_center_y, 0, (1, 0, 0), (0, 1, 0), gear_radius + 10, 180, 360)  # Change arc angle to negative for mirroring
+        line2 = create_line(gear_center_x + gear_radius + 10, y_position, gear_center_x + gear_radius + 10, gear_center_y, 0, 0)  # Keep x, Change y
+        base_lines = [
+            create_line(gear_center_x - gear_radius - 15, y_position, gear_center_x - gear_radius - 5, y_position, depth, depth),  # Keep x, Change y
+            create_line(gear_center_x - gear_radius - 5, y_position, gear_center_x - gear_radius - 5, y_position, depth, 0),  # Keep x, Change y
+            create_line(gear_center_x - gear_radius - 5, y_position, gear_center_x - gear_radius - 15, y_position, 0, 0),  # Keep x, Change y
+            create_line(gear_center_x - gear_radius - 15, y_position, gear_center_x - gear_radius - 15, y_position, 0, depth)  # Keep x, Change y
+        ]
+        swept = Swept([line1, arc_g1, line2], base_lines)
+
+        # Create second swept object
+        line3 = create_line(gear_center_x - gear_radius - 5, y_position, gear_center_x - gear_radius - 110, y_position, 0, 0)  # Keep x, Change y
+        base_lines = [
+            create_line(gear_center_x - gear_radius - 5, y_position - 10, gear_center_x - gear_radius - 5, y_position - 10, 0, depth),  # Keep x, Change y
+            create_line(gear_center_x - gear_radius - 5, y_position - 10, gear_center_x - gear_radius - 5, y_position, depth, depth),  # Keep x, Change y
+            create_line(gear_center_x - gear_radius - 5, y_position, gear_center_x - gear_radius - 5, y_position, depth, 0),  # Keep x, Change y
+            create_line(gear_center_x - gear_radius - 5, y_position, gear_center_x - gear_radius - 5, y_position - 10, 0, 0)  # Keep x, Change y
+        ]
+        swept2 = create_swept(line3, base_lines)
+
+        # Create third swept object
+        line4 = create_line(gear_center_x + gear_radius + 5, y_position, gear_center_x + gear_radius + 110, y_position, 0, 0)  # Keep x, Change y
+        base_lines = [
+            create_line(gear_center_x + gear_radius + 5, y_position - 10, gear_center_x + gear_radius + 5, y_position - 10, 0, depth),  # Keep x, Change y
+            create_line(gear_center_x + gear_radius + 5, y_position - 10, gear_center_x + gear_radius + 5, y_position, depth, depth),  # Keep x, Change y
+            create_line(gear_center_x + gear_radius + 5, y_position, gear_center_x + gear_radius + 5, y_position, depth, 0),  # Keep x, Change y
+            create_line(gear_center_x + gear_radius + 5, y_position, gear_center_x + gear_radius + 5, y_position - 10, 0, 0)  # Keep x, Change y
+        ]
+        swept3 = create_swept(line4, base_lines)
 
 
--- KBE/Python/gear_journal.py
+++ KBE/Python/gear_journal.py
@@ -1,8 +1,9 @@
 from Shapes.Gear import Gear
-from case import Case
+from Case import UpperCase, LowerCase
 
-gear1 = Gear(170, 10, 10, 40, 50, 100)
+gear1 = Gear(100, 20, 20, 40, 50, 100)
 gear2 = Gear(gear1.gearR, gear1.toothLength, gear1.toothWidth, gear1.gearHeight, gear1.x, gear1.y-2*gear1.gearR + gear1.toothLength*0.7)
 
 
-upper_case = Case(gear1.x, gear1.y, gear1.gearR, gear1.gearHeight)
+upper_case = UpperCase(gear1.x, gear1.y, gear1.gearR, gear1.gearHeight)
+lower_case = LowerCase(gear2.x, gear2.y, gear2.gearR, gear2.gearHeight)
