0e627c896194d22077459bbbc86abdc508f0e8b2
Martin Bergstø <mbergst@ntnu.no>
2023-09-29 12:41:37 +0200

    Made upper case

--- KBE/Python/case.py
+++ KBE/Python/case.py
@@ -37,45 +37,14 @@ class Case:
 
         swept2 = Swept([line3], [base_line1,base_line2,base_line3,base_line4])
 
-        # line4 = Line(110,-50,0,210,-50,0)
+        line4 = Line(gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5, 0, gear_center_x + gear_radius + 110, gear_center_y - gear_radius*0.5 ,0)
 
-        # base_line1 = Line(105,-40,0,105,-40, depth)
-        # base_line2 = Line(105,-40, depth,105,-50, depth )
-        # base_line3 = Line(105,-50, depth,105,-50,0)
-        # base_line4 = Line(105,-50,0,105,-40,0)
+        base_line1 = Line(gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5 + 10,0,gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5 + 10, depth)
+        base_line2 = Line(gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5 + 10, depth,gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5, depth )
+        base_line3 = Line(gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5, depth,gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5,0)
+        base_line4 = Line(gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5,0,gear_center_x + gear_radius +5,gear_center_y - gear_radius*0.5 + 10,0)
 
-        # swept3 = Swept([line4], [base_line1,base_line2,base_line3,base_line4])
+        swept3 = Swept([line4], [base_line1,base_line2,base_line3,base_line4])
 
-    # def __init__(self, x, y, z):
 
-    #     line1 = Line(-110,-50,0,-110,0,0)
-    #     arc_g1 = Arc(0,0,0,(1,0,0),(0,1,0),110,0, 180)
-    #     line2 = Line(110,-50,0,110,0,0)
-
-    #     base_line1 = Line(-115,-50,40,-105,-50,40)
-    #     base_line2 = Line(-105,-50,40,-105,-50,0)
-    #     base_line3 = Line(-105,-50,0,-115,-50,0)
-    #     base_line4 = Line(-115,-50,0,-115,-50,40)
-
-    #     swept = Swept([line1, arc_g1, line2], [base_line1,base_line2,base_line3,base_line4])
-
-    #     line3 = Line(-105,-50,0,-210,-50,0)
-
-    #     base_line1 = Line(-105,-40,0,-105,-40,40)
-    #     base_line2 = Line(-105,-40,40,-105,-50,40)
-    #     base_line3 = Line(-105,-50,40,-105,-50,0)
-    #     base_line4 = Line(-105,-50,0,-105,-40,0)
-
-    #     swept2 = Swept([line3], [base_line1,base_line2,base_line3,base_line4])
-
-    #     line4 = Line(110,-50,0,210,-50,0)
-
-    #     base_line1 = Line(105,-40,0,105,-40,40)
-    #     base_line2 = Line(105,-40,40,105,-50,40)
-    #     base_line3 = Line(105,-50,40,105,-50,0)
-    #     base_line4 = Line(105,-50,0,105,-40,0)
-
-    #     swept3 = Swept([line4], [base_line1,base_line2,base_line3,base_line4])
-
-        #swept.unite(swept2)
 
--- KBE/Python/gear_journal.py
+++ KBE/Python/gear_journal.py
@@ -1,4 +1,8 @@
 from Shapes.Gear import Gear
+from case import Case
 
-gear1 = Gear(70, 10, 10, 20, 0, 0)
-gear2 = Gear(gear1.gearR, gear1.toothLength, gear1.toothWidth, gear1.gearHeight, gear1.x, -2*gear1.gearR + gear1.toothLength*0.7)
+gear1 = Gear(170, 10, 10, 40, 50, 100)
+gear2 = Gear(gear1.gearR, gear1.toothLength, gear1.toothWidth, gear1.gearHeight, gear1.x, gear1.y-2*gear1.gearR + gear1.toothLength*0.7)
+
+
+upper_case = Case(gear1.x, gear1.y, gear1.gearR, gear1.gearHeight)
