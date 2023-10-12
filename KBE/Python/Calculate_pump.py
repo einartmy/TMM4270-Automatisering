b45df074164d2412604ca95980fa1ce747927af6
Martin Bergstø <mbergst@ntnu.no>
2023-10-12 15:56:10 +0200

    Added class calculate_pump

---
+++ KBE/Python/Calculate_pump.py
@@ -0,0 +1,37 @@
+import math
+
+class CalculatePump:
+    def __init__(self, radius, teeth_radius_ratio=5, angle_speed=1):
+        self.radius = radius
+        self.gear_radius = radius
+        self.teeth_radius = radius / teeth_radius_ratio
+        self.angle_speed = angle_speed
+
+    def number_of_teeth(self):
+        return math.ceil(2 * self.gear_radius * math.pi * 0.5 * (1 / self.teeth_radius))
+
+    def vpm(self):
+        depth = 2 * self.radius
+        teethradius = self.teeth_radius  # Use instance variable
+        angleSpeed = self.angle_speed  # Use instance variable
+        teeths = self.number_of_teeth()
+
+        angle = 2 * math.pi / teeths
+        korde = ((self.radius + teethradius) ** 2 * math.sin(angle)) / 2
+        areal = 4 * teethradius ** 2 + korde
+        volume = areal * depth
+
+        rpm = angleSpeed * 60 / (2 * math.pi * self.radius)  # Use instance variable
+        return volume * teeths * rpm * 2
+
+    def get_pump(self, target_vpm):
+        while target_vpm > self.vpm():
+            self.radius += 0.02
+
+# Example usage:
+if __name__ == "__main__":
+    initial_radius = 0.1
+    target_vpm = 1000  # Adjust this value as needed
+    pump = CalculatePump(initial_radius)
+    pump.get_pump(target_vpm)
+    print(f"Final radius to achieve {target_vpm} VPM: {pump.radius}")
--- KBE/Python/Pump_journal.py
+++ KBE/Python/Pump_journal.py
@@ -2,8 +2,16 @@ from Shapes.Pump_Gears import Pump_Gears
 from Case_parts import upperCase, lowerCase
 from Motion.Motion3 import Motion3
 import random
+from Calculate_pump import CalculatePump
 
-radius = 100
+
+target_vpm = 10
+
+pump = CalculatePump(0.1)
+pump.get_pump(target_vpm)
+radius = pump.radius * 1000  #Convert to mm
+
+
 depth = radius
 teethradius = radius/5
 x = 0
@@ -16,7 +24,7 @@ gear2 = Pump_Gears(radius, depth, teethradius, x, y - 2*radius, True)
 case1 = upperCase(radius, depth, teethradius, caseThickness, x, y)
 case2 = lowerCase(radius, depth, teethradius, caseThickness, x, y - 2*radius)
 
-pathToTheFolder = "M:\\Desktop\\TMM4270\\TMM4270-Automatisering\\KBE\\Python\\Animation\\"
-fileName = "geartrain_" + str(random.randint(1,10000)) 
+# pathToTheFolder = "M:\\Desktop\\TMM4270\\TMM4270-Automatisering\\KBE\\Python\\Animation\\"
+# fileName = "geartrain_" + str(random.randint(1,10000)) 
 
-motion = Motion3([gear1.gear, gear2.gear], 0, 10, 10, 0, True, pathToTheFolder, fileName)
+# motion = Motion3([gear1.gear, gear2.gear], 0, 10, 10, 0, True, pathToTheFolder, fileName)
