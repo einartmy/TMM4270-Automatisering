9b5ddc542ec7b7c1db369a63a95aea40d973b1af
mbergsto <mbergst@stud.ntnu.no>
2023-10-18 16:25:20 +0200

    Created folder for txt files with the each pump design

--- KBE/Python/Pump_journal.py
+++ KBE/Python/Pump_journal.py
@@ -2,9 +2,11 @@
 # from Case_parts import UpperCase, LowerCase
 # from Motion.Motion3 import Motion3
 # import random
+from datetime import datetime
 from Calculate_pump import CalculatePump
 import json
 import os
+import pandas as pd
 
 class Pump:
 
@@ -35,16 +37,28 @@ class Pump:
         # case2 = LowerCase(radius, depth, teethradius, self.case_thickness, self.x, self.y - 2*radius)
 
     def get_design_parameters(self):
-        return {
-            "targetVpm": self.targetVpm,
-            "case_thickness": self.case_thickness,
-            "x": self.x,
-            "y": self.y,
-            "calculated_radius": round(self.radius, 5),
-            "calculated_depth": round(self.depth, 5),
-            "calculated_teethradius": round(self.teeth_radius, 5),
-            # ... [any other parameters you want to save] ...
-        }
+        data = {
+            "metadata": {
+            "timestamp": str(datetime.now().strftime("%Y-%m-%d_%H:%M:%S")),
+            "description": "Design parameters for the pump.",
+            },
+            "volumePerMinuteTarget": {
+                "value": self.targetVpm,
+                "unit": "cubic meters per minute",
+                "comment": "Desired volume throughput of the pump"
+            },
+            "parameters": {
+                "unit": "millimeters",
+                "calculated radius": round(self.radius, 5),
+                "calculated depth": round(self.depth, 5),
+                "calculated teethradius": round(self.teeth_radius, 5),
+                "casing thickness": self.case_thickness,
+                "x position (center of upper gear)": self.x,
+                "y position (center of upper gear)": self.y,
+            }
+            }
+        
+        return data
 
 if __name__ == "__main__":
     with open("KBE/Python/Pump_parameters.json", "r") as file:
@@ -57,14 +71,31 @@ if __name__ == "__main__":
     design_params = pump.get_design_parameters()
 
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    output_file_path = os.path.join(current_directory, "Design_parameters_output.json")
+    output_json_path = os.path.join(current_directory, "Design_parameters_output.json")
 
-    # Save the design parameters to a JSON output file
-    with open(output_file_path, "w") as file:
+    # Save the design parameters to a constant JSON output file
+    with open(output_json_path, "w") as file:
         json.dump(design_params, file, indent=4)
 
-    # targetVpm = 5  # Adjust this value as needed
-    # Pump(targetVpm)
+    #Save the design parameters to a new txt file each time a pump design is made
+    current_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    
+    # Name of the directory where you want to save your txt files
+    directory_name = "txt_output_files"
+    output_folder_path = os.path.join(current_directory, directory_name)
+
+    # Check if the directory exists. If not, create it.
+    if not os.path.exists(output_folder_path):
+        os.makedirs(output_folder_path)
+    
+    # Create unique filename with the timestamp and directory
+    filename = os.path.join(output_folder_path, f"Desing_paramaters_output_{current_time}.txt")
+    
+    # Save the data in the txt file
+    with open(filename, 'w') as outfile:
+        json.dump(design_params, outfile, indent=4)
+            
+
 
 
 
