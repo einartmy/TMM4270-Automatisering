98bd7808065ab2db5e0fbb19579a5e6fadba9884
mbergsto <mbergst@stud.ntnu.no>
2023-11-08 13:04:33 +0100

    working on queries for pump

--- KBE/Python/app.py
+++ KBE/Python/app.py
@@ -13,51 +13,123 @@ def index():
 @app.route("/create-pump", methods=["POST"])
 def calculate():
     target_vpm = float(request.form["targetVPM"])
+
+    # Sjekk om pumpen allerede eksisterer
+    if pump_exists(target_vpm):
+        # Hvis pumpen eksisterer, hent detaljene
+        #pump_details = get_pump_details(target_vpm)  #Må lage en funksjon som henter ut detaljene til pumpen
+        results = f"""
+        A pump with the target VPM {target_vpm} already exists:<br>
+        """
+    else:
+        #Beregninger basert på targetVPM
+        optimizer = GeneticPumpOptimizer(target_vpm)
+        best_pump = optimizer.run()
+        
+        # Legg til resultatene i en streng 
+        results = f"""
+        Optimized parameters to achieve close to {target_vpm} VPM are:<br>
+        Gear Radius: {round(best_pump.radius*1000, 4)} mm<br>
+        Teeth Diameter Ratio: {round(best_pump.teethDiameterRatio, 2)}<br>
+        Teeth Diameter: {round(best_pump.teethDiameter*1000, 4)} mm<br>
+        Angle Speed: {round(best_pump.angleSpeed, 2)} rad/s<br>
+        Depth: {round(best_pump.depth*1000, 4)} mm<br>
+        Number of Teeth: {best_pump.numberOfTeeth()}<br>
+        Calculated VPM: {round(best_pump.vpm(), 2)}<br>
+        """
     
-    #Beregninger basert på targetVPM
-    optimizer = GeneticPumpOptimizer(target_vpm)
-    best_pump = optimizer.run()
-    
-    # Legg til resultatene i en streng 
-    results = f"""
-    Optimized parameters to achieve close to {target_vpm} VPM are:<br>
-    Radius: {round(best_pump.radius*1000, 4)} mm<br>
-    Teeth Diameter Ratio: {round(best_pump.teethDiameterRatio, 2)}<br>
-    Teeth Diameter: {round(best_pump.teethDiameter*1000, 4)} mm<br>
-    Angle Speed: {round(best_pump.angleSpeed, 2)} rad/s<br>
-    Depth: {round(best_pump.depth*1000, 4)} mm<br>
-    Number of Teeth: {best_pump.numberOfTeeth()}<br>
-    Calculated VPM: {round(best_pump.vpm(), 2)}<br>
-    """
-    
-    # Run all update functions
-    update_data()
+        # Run all update functions
+        insert_data(target_vpm)
+        get_data()
     
     return results
 
 
-def update_sparql_data(sparql_query):
-    endpoint_url = "http://localhost:3030/#/dataset/A3/update"  
+def get_sparql_data(sparql_query):
+    url = "http://localhost:3030/A3/query"
     
-    response = requests.post(endpoint_url, data={"update": sparql_query})
-    
+    PARAMS = {"query": sparql_query}
+
+    response = requests.get(url, PARAMS)
+    data = response.json()
+    print(data)
+
     if response.status_code == 200:
         return "Update successful!"
     else:
         return f"Failed with status code: {response.status_code}. Message: {response.text}"
 
-def update_data():
+def get_data():
     sparql_query = """
     PREFIX A3: <http://www.kbe.com/pump.owl#>
-    INSERT DATA {
-    A3:Pump{targetVPM} a A3:Pump ;
-                        A3:name "pump{targetVPM}" .
-}
+    SELECT ?pump
+    WHERE {
+	?pump a A3:Pump.
+    }
     """
-    
-    result = update_sparql_data(sparql_query)
+    result = get_sparql_data(sparql_query)
     return result
+
+def insert_data(target_vpm):
+    sparql_query = f"""
+    PREFIX A3: <http://www.kbe.com/pump.owl#>
+    INSERT {{
+        A3:pump_{get_pump_count()} a A3:Pump;
+            A3:targetVPM {target_vpm}.
+    }}
+    WHERE {{
+    }}
+    """
+    result = insert_sparql_data(sparql_query)
+    return result
+
+def get_pump_count():
+    sparql_query = """
+    PREFIX A3: <http://www.kbe.com/pump.owl#>
+    SELECT (COUNT(?pump) AS ?count)
+    WHERE {
+        ?pump a A3:Pump.
+    }
+    """
+    url = "http://localhost:3030/A3/query"
+    PARAMS = {"query": sparql_query}
+    response = requests.get(url, PARAMS)
+    data = response.json()
+    count = int(data["results"]["bindings"][0]["count"]["value"])
+    return count + 1
+
+def insert_sparql_data(sparql_query):
+    url = "http://localhost:3030/A3/update"
+    
+    PARAMS = {"update": sparql_query}
+
+    response = requests.post(url, PARAMS)
+
+    if response.status_code == 200:
+        return "Update successful!"
+    else:
+        return f"Failed with status code: {response.status_code}. Message: {response.text}"
+
+def pump_exists(target_vpm):
+    # SPARQL Query to check if a Pump with the given targetVPM exists
+    sparql_query = f"""
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX A3: <http://www.kbe.com/pump.owl#>
+    ASK {{
+        ?pump a A3:Pump ;
+              A3:targetVPM "{target_vpm}"^^xsd:decimal.
+    }}
+    """
+    url = "http://localhost:3030/A3/query"
+    PARAMS = {"query": sparql_query}
+    response = requests.get(url, PARAMS)
+    if response.status_code == 200:
+        data = response.json()
+        return data["boolean"]  # This will be True if the Pump exists, False otherwise
+    else:
+        raise Exception(f"Query failed with status code: {response.status_code}. Message: {response.text}")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
+
 
